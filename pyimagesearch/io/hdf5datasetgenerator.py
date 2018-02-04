223baba809d9d35123dfa47a33cf5a6307ebae88
msharp9 <msharp9@outlook.com>
2018-02-04 16:10:41 -0700

    Updated dataset generator to accept model and train/val tag, started to set up train model as well

--- pyimagesearch/io/hdf5datasetgenerator.py
+++ pyimagesearch/io/hdf5datasetgenerator.py
@@ -5,7 +5,7 @@ import h5py
 
 class HDF5DatasetGenerator:
 	def __init__(self, dbPath, batchSize, preprocessors=None,
-		aug=None, binarize=True, classes=2):
+		aug=None, binarize=True, model=None, set=None, classes=2):
 		# store the batch size, preprocessors, and data augmentor,
 		# whether or not the labels should be binarized, along with
 		# the total number of classes
@@ -13,12 +13,20 @@ class HDF5DatasetGenerator:
 		self.preprocessors = preprocessors
 		self.aug = aug
 		self.binarize = binarize
+		self.model = model
 		self.classes = classes
 
 		# open the HDF5 database for reading and determine the total
 		# number of entries in the database
 		self.db = h5py.File(dbPath)
-		self.numImages = self.db["labels"].shape[0]
+		if set == "train":
+			self.numImages = int(self.db["labels"].shape[0]*0.75)
+		else:
+			self.numImages = self.db["labels"].shape[0]
+		if set == "val":
+			self.startImages = int(self.numImages*0.75)
+		else:
+			self.startImages = 0
 
 	def generator(self, passes=np.inf):
 		# initialize the epoch count
@@ -28,7 +36,7 @@ class HDF5DatasetGenerator:
 		# reach the desired number of epochs
 		while epochs < passes:
 			# loop over the HDF5 dataset
-			for i in np.arange(0, self.numImages, self.batchSize):
+			for i in np.arange(self.startImages, self.numImages, self.batchSize):
 				# extract the images and labels from the HDF dataset
 				images = self.db["images"][i: i + self.batchSize]
 				labels = self.db["labels"][i: i + self.batchSize]
@@ -61,6 +69,10 @@ class HDF5DatasetGenerator:
 				if self.aug is not None:
 					(images, labels) = next(self.aug.flow(images,
 						labels, batch_size=self.batchSize))
+
+				# if model is provided, return features instead
+				if self.model is not None:
+					images = self.model.predict(images, batch_size=self.batchSize)
 
 				# yield a tuple of images and labels
 				yield (images, labels)
--- train_model.py
+++ train_model.py
@@ -1,37 +1,82 @@
 # USAGE
 # python train_model.py --db ../datasets/kaggle_dogs_vs_cats/hdf5/features.hdf5 --model dogs_vs_cats.pickle
 
+import matplotlib
+matplotlib.use("Agg")
+
 # import the necessary packages
+from config import config
+from pyimagesearch.callbacks import TrainingMonitor
+from pyimagesearch.io import HDF5DatasetGenerator
+from pyimagesearch.preprocessing import ImageToArrayPreprocessor
+from keras.applications import xception
+from keras.applications import imagenet_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
-import argparse
+import numpy as np
 import pickle
 import h5py
+import os
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--db", required=True,
-	help="path HDF5 database")
-ap.add_argument("-m", "--model", required=True,
-	help="path to output model")
-ap.add_argument("-j", "--jobs", type=int, default=-1,
-	help="# of jobs to run when tuning hyperparameters")
-args = vars(ap.parse_args())
+
+# construct the training image generator for data augmentation
+aug = ImageDataGenerator(preprocessing_function=xception.preprocess_input,
+	rotation_range=30, zoom_range=0.2,
+	width_shift_range=0.2, height_shift_range=0.2, shear_range=0.1,
+	horizontal_flip=True, fill_mode="nearest")
 
 # open the HDF5 database for reading then determine the index of
 # the training and testing split, provided that this data was
 # already shuffled *prior* to writing it to disk
-db = h5py.File(args["db"], "r")
+db = h5py.File(config.TRAIN_HDF5, "r")
 i = int(db["labels"].shape[0] * 0.75)
+
+iap = ImageToArrayPreprocessor()
+# initialize the training and validation dataset generators
+trainGen = HDF5DatasetGenerator(config.TRAIN_HDF5, 64, aug=aug,
+	preprocessors=[pp, mp, iap], classes=2)
+valGen = HDF5DatasetGenerator(config.VAL_HDF5, 64,
+	preprocessors=[sp, mp, iap], classes=2)
+
+image = np.expand_dims(image, axis=0)
+image = imagenet_utils.preprocess_input(image)
+x = xception.preprocess_input(np.expand_dims(img.copy(), axis=0))
+POOLING = 'avg'
+xception_bottleneck = xception.Xception(weights='imagenet', include_top=False, pooling=POOLING)
+
+# construct the set of callbacks
+path = os.path.sep.join([config.OUTPUT_PATH, "{}.png".format(
+	os.getpid())])
+callbacks = [TrainingMonitor(path)]
+
+# train the network
+model.fit_generator(
+	trainGen.generator(),
+	steps_per_epoch=trainGen.numImages // 64,
+	validation_data=valGen.generator(),
+	validation_steps=valGen.numImages // 64,
+	epochs=50,
+	max_q_size=64 * 2,
+	callbacks=callbacks, verbose=1)
+
+# save the model to file
+print("[INFO] serializing model...")
+model.save(config.MODEL_PATH, overwrite=True)
+
+# close the HDF5 datasets
+trainGen.close()
+valGen.close()
 
 # define the set of parameters that we want to tune then start a
 # grid search where we evaluate our model for each value of C
 print("[INFO] tuning hyperparameters...")
 params = {"C": [0.0001, 0.001, 0.01, 0.1, 1.0]}
 model = GridSearchCV(LogisticRegression(), params, cv=3,
-	n_jobs=args["jobs"])
+	n_jobs=-1)
 model.fit(db["features"][:i], db["labels"][:i])
 print("[INFO] best hyperparameters: {}".format(model.best_params_))
 
@@ -47,7 +92,7 @@ print("[INFO] score: {}".format(acc))
 
 # serialize the model to disk
 print("[INFO] saving model...")
-f = open(args["model"], "wb")
+f = open(config.MODEL_PATH, "wb")
 f.write(pickle.dumps(model.best_estimator_))
 f.close()
 
